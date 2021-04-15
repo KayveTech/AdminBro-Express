e3d1635d4904d34d0699e5485c17e3afa4ddf7b0
Kevin Kimani <77485010+KayveTech@users.noreply.github.com>
2021-04-15 12:01:28 +0300

    Update and rename index.js to server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+
+var express = require('express');
+
+const AdminBro = require('admin-bro');
+const AdminBroMongoose = require('@admin-bro/mongoose');
+const AdminBroExpress = require('@admin-bro/express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+
+
+// Database
+const connection = require('./config/db.config');
+
+connection.once('open', ()=>console.log('Database connected Successfully'));
+connection.on('error',()=>console.log('Error', err));
+
+//Admin Bro and Models
+const Customer = require('./models/Customer')
+const User = require('./models/User')
+
+AdminBro.registerAdapter(AdminBroMongoose)
+const AdminBroOptions = {
+  resources: [User, Customer],
+}
+
+const adminBro = new AdminBro(AdminBroOptions)
+const router = AdminBroExpress.buildRouter(adminBro)
+
+app.use(adminBro.options.rootPath, router)
+app.listen(8000, function () {
+    console.log('Listening to Port 8000');
+});
