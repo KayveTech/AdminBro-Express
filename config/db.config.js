030582dc4334a033772c00759d108ebb2a892d9d
Kevin Kimani <77485010+KayveTech@users.noreply.github.com>
2021-05-06 20:35:45 +0300

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose')
-
+const DB_URI = 'mongodb:localhost:27017/AdminBro';
 //Database connection
-mongoose.connect('mongodb://localhost/AdminBro',
+mongoose.connect(DB_URI,
 {useNewUrlParser: true, useUnifiedTopology: true});
-
 const connection = mongoose.connection
-
 module.exports = connection
